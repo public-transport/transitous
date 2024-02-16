14e164d0c36d65efddcbb97c1e2b68cc6ba2d756
Jonah Brüchert <jbb@kaidan.im>
2024-02-16 22:11:45 +0100

    Add script to generate a motis config file

---
+++ ci/generate-motis-config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Jonah Brüchert <jbb@kaidan.im>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import subprocess
+import json
+import metadata
+import tomllib
+
+from pathlib import Path
+from jinja2 import Template
+
+
+if __name__ == "__main__":
+    feed_dir = Path("feeds/")
+    osm_map = "berlin-latest.osm.pbf"
+
+    gtfs_feeds = []
+
+    for feed in feed_dir.glob("*.json"):
+        with open(feed, "r") as f:
+            parsed = json.load(f)
+            region = metadata.Region(parsed)
+
+            metadata_filename = feed.name
+            region_name = metadata_filename[:metadata_filename.rfind('.')]
+
+            for source in region.sources:
+                schedule_name = f"{region_name}-{source.name}"
+                schedule_file = f"{region_name}_{source.name}.gtfs.zip"
+
+                gtfs_feeds.append({
+                    "id": schedule_name,
+                    "path": schedule_file
+                })
+
+    with open("motis/config.ini.j2") as f:
+        template = Template(f.read())
+        print(template.render(gtfs_feeds=gtfs_feeds, pbf_file=osm_map))
