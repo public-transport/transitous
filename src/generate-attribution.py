c0c1947550ebcf50d4d73f14debd495ba3de316f
Lars Röglin <git@parttimedatascientist.de>
2024-03-05 22:59:13 +0000

    Auto-generate licenses.md in generate-attribution.py

--- src/generate-attribution.py
+++ src/generate-attribution.py
@@ -18,6 +18,11 @@ if __name__ == "__main__":
 
     transitland_atlas = transitland.Atlas.load(Path("transitland-atlas/"))
 
+    markdown = """
+# Licenses of included feeds
+
+"""
+
     attributions = []
 
     for feed in feed_dir.glob("*.json"):
@@ -49,6 +54,7 @@ if __name__ == "__main__":
             feed_path = Path(f"out/{region_name}_{source.name}.gtfs.zip")
 
             attribution["filename"] = feed_path.name
+            markdown += "## Filename: " + feed_path.name + "  \r\n"
 
             if not feed_path.exists():
                 print(f"Info: {feed_path} does not exist, skipping…")
@@ -59,11 +65,23 @@ if __name__ == "__main__":
                     with io.TextIOWrapper(a) as at:
                         agencyreader = \
                             csv.DictReader(at, delimiter=',', quotechar='"')
+                        markdown += "### Copyright holders  \r\n"
                         for row in agencyreader:
                             attribution["copyright_holders"] \
                                 .append(row["agency_name"])
+                            markdown += " * " + row["agency_name"] + "  \r\n"
 
             attributions.append(attribution)
 
     with open("out/license.json", "w") as outfile:
         json.dump(attributions, outfile, indent=4, ensure_ascii=False)
+    
+
+    markdown += """
+<!--
+SPDX-FileCopyrightText: None
+SPDX-License-Identifier: CC0-1.0
+-->
+"""
+    with open("mkdocs-site/docs/licenses.md", "w") as outfile:
+        outfile.write(markdown)
