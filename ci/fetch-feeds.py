fb0eb24ec0665259b8912baea87ad231ef0ad093
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-03-19 22:51:53 +0100

    create github issue for failed imports if token provided

--- ci/fetch-feeds.py
+++ ci/fetch-feeds.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import subprocess
 import multiprocessing
 import concurrent.futures
+import requests
 
 if len(sys.argv) < 2:
     print("Argument run-reason is missing.")
@@ -17,6 +18,53 @@ if len(sys.argv) < 2:
 
 run_reason = sys.argv[1]
 feed_dir = Path("feeds/")
+repo = "public-transport/transitous"
+
+
+def create_feed_error_issue(feed: str, details: str, github_token: str) -> None:
+    issue_title = f"Error fetching '{feed}'"
+
+    # Check if an error issue already exists
+    response = requests.get(
+        "https://api.github.com/search/issues",
+        headers={"Authorization": f"Bearer {github_token}"},
+        params={"q": f"repo:{repo} is:issue {issue_title}"},
+    )
+
+    if response.status_code < 200 or response.status_code >= 300:
+        print(f"Error searching for existing issue for {feed}")
+        print(response.json())
+        return
+
+    if response.json()["total_count"] > 0:
+        if issue["title"] == issue_title:
+            # Update the issue with the new error
+            issue_number = response.json()["items"][0]["number"]
+            requests.patch(
+                f"https://api.github.com/repos/{repo}/issues/{issue_number}",
+                headers={"Authorization": f"Bearer {github_token}"},
+                json={"body": f"{issue['body']}\n\n```{details}```"},
+            )
+            print(f"Updated error issue for {feed}")
+            return
+
+    # Create an issue on the repository
+    response = requests.post(
+        f"https://api.github.com/repos/{repo}/issues",
+        headers={"Authorization": f"Bearer {github_token}"},
+        json={
+            "title": issue_title,
+            "body": f"Error fetching {feed}\n\n```{details}```",
+            "labels": ["import-issue"],
+        },
+    )
+
+    if response.status_code < 200 or response.status_code >= 300:
+        print(f"Error creating issue for {feed}")
+        print(response.json())
+        return
+
+    print(f"Created error issue for {feed}")
 
 
 def do_fetch(feed: str):
@@ -24,7 +72,11 @@ def do_fetch(feed: str):
         subprocess.check_call(["./src/fetch.py", feed])
     except:
         print(f"Error fetching {feed}")
-        # TODO: create an issue on the repository
+        if "GITHUB_TOKEN" in os.environ:
+            details = subprocess.check_output(
+                ["./src/fetch.py", feed], stderr=subprocess.STDOUT
+            ).decode()
+            create_feed_error_issue(feed, details, os.environ["GITHUB_TOKEN"])
 
 
 match run_reason:
