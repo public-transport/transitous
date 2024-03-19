ea32583887214215a046021f71f3bd902cd0c359
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-03-19 22:33:44 +0100

    adjust code

--- ci/fetch-feeds.py
+++ ci/fetch-feeds.py
@@ -18,6 +18,7 @@ if len(sys.argv) < 2:
 run_reason = sys.argv[1]
 feed_dir = Path("feeds/")
 
+
 def do_fetch(feed: str):
     try:
         subprocess.check_call(["./src/fetch.py", feed])
@@ -25,14 +26,18 @@ def do_fetch(feed: str):
         print(f"Error fetching {feed}")
         # TODO: create an issue on the repository
 
+
 match run_reason:
     case "timer":
         json_files = list(feed_dir.glob("*.json"))
 
-        max_workers = multiprocessing.cpu_count()
-        with concurrent.futures.ThreadPoolExecutor(max_workers) as executor:
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=multiprocessing.cpu_count()
+        ) as executor:
             for index, feed in enumerate(json_files):
-                print(f'PROGRESS: Processing file {index+1} of {len(json_files)} | {index/len(json_files)*100:.1f}% done...')
+                print(
+                    f"PROGRESS: Processing file {index+1} of {len(json_files)} | {index/len(json_files)*100:.1f}% done..."
+                )
                 sys.stdout.flush()
                 executor.submit(do_fetch, feed)
 
@@ -41,20 +46,30 @@ match run_reason:
         # Silence warnings about different ownerships inside and outside of the
         # container on actions
         subprocess.check_call(
-            ["git", "config", "--global",
-             "--add", "safe.directory", os.getcwd()])
+            ["git", "config", "--global", "--add", "safe.directory", os.getcwd()]
+        )
 
         # Find all files that were changed in the latest commit
-        changed_files = subprocess.check_output(
-            ["git", "diff", "--name-only", "origin/main", "HEAD"]) \
-            .decode().splitlines()
+        changed_files = (
+            subprocess.check_output(
+                ["git", "diff", "--name-only", "origin/main", "HEAD"]
+            )
+            .decode()
+            .splitlines()
+        )
 
-        changed_feeds = [f for f in changed_files if Path(f).exists() and
-                         f.startswith("feeds/") and f.endswith(".json")]
+        changed_feeds = [
+            f
+            for f in changed_files
+            if Path(f).exists() and f.startswith("feeds/") and f.endswith(".json")
+        ]
 
-        max_workers = 4
-        with concurrent.futures.ThreadPoolExecutor(max_workers) as executor:
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=multiprocessing.cpu_count()
+        ) as executor:
             for index, feed in enumerate(changed_feeds):
-                print(f'PROGRESS: Processing file {index+1} of {len(changed_feeds)} | {index/len(changed_feeds)*100:.1f}% done...')
+                print(
+                    f"PROGRESS: Processing file {index+1} of {len(changed_feeds)} | {index/len(changed_feeds)*100:.1f}% done..."
+                )
                 sys.stdout.flush()
                 executor.submit(do_fetch, feed)
