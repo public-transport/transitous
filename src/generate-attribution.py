97fd22dd5dcc54c638be8fb5fd887fda3190d1fc
Jonah Brüchert <jbb@kaidan.im>
2024-09-14 11:26:48 +0200

    Improve display of sources on the website

--- src/generate-attribution.py
+++ src/generate-attribution.py
@@ -7,11 +7,15 @@ import json
 import csv
 import io
 import transitland
+import pycountry
 
 from pathlib import Path
 from metadata import Source, TransitlandSource, Region
 from zipfile import ZipFile
 from typing import Optional
+
+
+REGIONS = {"EU": "European Union"}
 
 
 if __name__ == "__main__":
@@ -28,7 +32,7 @@ if __name__ == "__main__":
 
         region = Region(parsed)
         for source in region.sources:
-            if (type(source) == TransitlandSource):
+            if type(source) == TransitlandSource:
                 source = transitland_atlas.source_by_id(source)
                 if not source:
                     continue
@@ -43,8 +47,7 @@ if __name__ == "__main__":
 
             if source.license:
                 if source.license.spdx_identifier:
-                    attribution["spdx_license_identifier"] \
-                        = source.license.spdx_identifier
+                    attribution["spdx_license_identifier"] = source.license.spdx_identifier
                 if source.license.url:
                     attribution["license_url"] = source.license.url
 
@@ -52,11 +55,15 @@ if __name__ == "__main__":
             attribution["source"] = source.url
 
             metadata_filename = feed.name
-            region_name = metadata_filename[:metadata_filename.rfind('.')]
+            region_name = metadata_filename[: metadata_filename.rfind(".")]
 
             feed_path = Path(f"out/{region_name}_{source.name}.gtfs.zip")
 
             attribution["filename"] = feed_path.name
+
+            human_name: str = feed_path.name.replace(".gtfs.zip", "").split("_")[1].replace("-", " ")
+            human_name = " ".join(map(lambda w: w[0].upper() + w[1:] if len(w) > 0 else w, human_name.split(" ")))
+            attribution["human_name"] = human_name
 
             if not feed_path.exists():
                 print(f"Info: {feed_path} does not exist, skipping…")
@@ -65,11 +72,30 @@ if __name__ == "__main__":
             with ZipFile(feed_path) as z:
                 with z.open("agency.txt", "r") as a:
                     with io.TextIOWrapper(a) as at:
-                        agencyreader = \
-                            csv.DictReader(at, delimiter=',', quotechar='"')
+                        agencyreader = csv.DictReader(at, delimiter=",", quotechar='"')
                         for row in agencyreader:
-                            attribution["operators"] \
-                                .append(row["agency_name"])
+                            attribution["operators"].append(row["agency_name"])
+
+            if (
+                "operators" in attribution
+                and len(attribution["operators"]) == 1
+                and len(attribution["operators"][0]) > 1
+            ):
+                attribution["human_name"] = attribution["operators"][0]
+
+            region_code = feed_path.name.split("_")[0].upper()
+            attribution["region_code"] = region_code
+            region_name = ""
+            subdivision = pycountry.subdivisions.get(code=region_code)
+            if not subdivision:
+                country = pycountry.countries.get(alpha_2=region_code)
+                if country:
+                    region_name = country.name
+                else:
+                    region_name = REGIONS[region_code]
+            else:
+                region_name = subdivision.name
+            attribution["region_name"] = region_name
 
             attributions.append(attribution)
 
