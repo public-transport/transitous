dbead4e5cc2845af13524cb5a67057bc6fc3965b
Jonah Brüchert <jbb@kaidan.im>
2024-12-28 20:42:21 +0100

    tmp

--- ansible/roles/rsync-push/files/motis-update-feeds.sh
+++ ansible/roles/rsync-push/files/motis-update-feeds.sh
@@ -3,16 +3,5 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-echo "Copying new files…"
 
-# Exit if empty
-if [ -z "$(ls -A /srv/rsync/transitous/)" ]; then
-    exit 0
-fi
-
-rm /var/lib/motis/data/nigiri/ -rf
-rsync --progress -r /srv/rsync/transitous/ /var/lib/motis/
-chown motis:motis -R /var/lib/motis/data/
-
-echo "Restarting MOTIS…"
 systemctl --no-ask-password restart motis.service
