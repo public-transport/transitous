1ca10c62e84854249fb627a96e28f057f9e87ff4
Jonah Brüchert <jbb@kaidan.im>
2025-01-15 10:24:49 +0100

    Restore simplified motis-update-feeds.sh

--- ansible/roles/rsync-push/files/motis-update-feeds.sh
+++ ansible/roles/rsync-push/files/motis-update-feeds.sh
@@ -3,5 +3,14 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+echo "Copying new files…"
 
+# Exit if empty
+if [ -z "$(ls -A /srv/rsync/transitous/)" ]; then
+    exit 0
+fi
+
+rsync --progress -r /srv/rsync/transitous/ /var/lib/motis/
+
+echo "Restarting MOTIS…"
 systemctl --no-ask-password restart motis.service
