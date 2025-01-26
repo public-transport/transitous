47f0362833248e0d81003b883a5af9637aefa980
Jonah Brüchert <jbb@kaidan.im>
2025-01-26 22:29:11 +0100

    Adapt to import happening on the same host

--- ansible/roles/motis/files/motis-update-feeds.sh
+++ ansible/roles/motis/files/motis-update-feeds.sh
@@ -6,11 +6,16 @@
 echo "Copying new files…"
 
 # Exit if empty
-if [ -z "$(ls -A /srv/rsync/transitous/)" ]; then
+if [ -z "$(ls -A /var/cache/transitous/out)" ]; then
     exit 0
 fi
 
-rsync -a --progress -r /srv/rsync/transitous/ /var/lib/motis/
+if [ -f /var/cache/transitous/out/.import-running ]; then
+    echo "Import has not finished, exiting"
+    exit 0
+fi
+
+rsync -a --progress -r /var/cache/transitous/out /var/lib/motis/
 
 echo "Restarting MOTIS…"
 systemctl --no-ask-password restart motis.service
