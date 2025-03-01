6e8aa3f64ae0c052a51f5b3560adf5629ae1bb2c
Jonah Brüchert <jbb@kaidan.im>
2025-03-02 00:46:20 +0100

    fetch: Don't merge identical routes and stops (#857)
    Could interfere with gtfs-rt matching

--- src/fetch.py
+++ src/fetch.py
@@ -326,7 +326,7 @@ class Fetcher:
                    "--fix-zip",
                    "--check-null-coords",
                    "--empty-agency-url-repl", "https://transitous.org",
-                   "--remove-red-routes", "--remove-red-services", "--remove-red-stops",
+                   "--remove-red-services",
                    "--output", str(temp_file)]
         if source.fix:
             command.append("--fix")
