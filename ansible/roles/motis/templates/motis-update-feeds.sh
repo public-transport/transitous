00aabf3bb11ce9489b3ffa997601ef047d2d9676
Traines <git@traines.eu>
2025-05-25 14:57:03 +0000

    goldberg

--- ansible/roles/motis/templates/motis-update-feeds.sh
+++ ansible/roles/motis/templates/motis-update-feeds.sh
@@ -5,10 +5,15 @@
 
 echo "Copying new files…"
 
-{% if not motis_primary %}
+{% if motis_primary is not defined or not motis_primary %}
+
+{% if motis_no_cow %}
+#cp -r -p /var/lib/motis/data /var/cache/transitous/out/data
+sudo -u motis cp -r -p /var/cache/transitous/out/osm/ /var/cache/transitous/out/data/ # TODO -l
+{% endif %}
 
 cd /var/cache/transitous/out/
-sudo -u motis wget --mirror --no-parent -e robots=off https://api.transitous.org/gtfs/data/ || true
+sudo -u motis wget -N -nH --cut-dirs=1 --exclude-directories=/gtfs/data/tiles/,/gtfs/data/osr/ --mirror --no-parent -e robots=off https://api.transitous.org/gtfs/data/ || true
 
 {% endif %}
 
@@ -23,10 +28,16 @@ if [ -f /var/cache/transitous/out/.import-running ]; then
     exit 0
 fi
 
+{% if motis_no_cow %}
+rm -r /var/lib/motis/data/
+mv /var/cache/transitous/out/data /var/lib/motis/
+{% else %}
 cp -r -u --reflink=auto /var/cache/transitous/out/data /var/lib/motis/
+{% endif %}
+
 cp --reflink=auto /var/cache/transitous/out/config.yml /var/lib/motis/data/config.yml
 
-chown -R www-data:www-data /var/lib/motis/data/
+chown -R motis:www-data /var/lib/motis/data/
 chmod -R u+r,g+r /var/lib/motis/data/
 
 echo "Restarting MOTIS…"
