fe949575ae79aabf0a4813b9002e06e7102fa464
Marcin Kasznia <kasmar00@gmail.com>
2025-07-29 20:22:17 +0200

    Dynamic link for Gorzow Wlkp, add Olsztyn (#1390)

--- src/region_helpers.py
+++ src/region_helpers.py
@@ -45,3 +45,23 @@ def delhi_gov_in_csrf(source: HttpSource) -> HttpSource:
     source.options.headers["Cookie"] = f"csrftoken={csrftoken}"
     source.options.request_body = f"csrfmiddlewaretoken={csrftoken}"
     return source
+
+
+def data_mzk_gorzow_latest_resource(source: HttpSource) -> HttpSource:
+    from bs4 import BeautifulSoup
+
+    url = "https://bip.mzk-gorzow.com.pl/gtfs.html"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    } # user-agent is necessary to avoid 403 Forbidden
+
+    html = requests.get(url, headers=headers).text
+
+    soup = BeautifulSoup(html, "lxml")
+    gtfs_link = soup.find("a", href=lambda x: x and "gtfs" in x and x.endswith(".zip"))['href']
+    
+    base_url = url.rsplit("/", 1)[0]
+    url = f"{base_url}/{gtfs_link}"
+    
+    source.url = url
+    return source
