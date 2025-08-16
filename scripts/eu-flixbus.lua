b8f808a7156abe0b987fcfc84757b7c62b49ea5d
Volker Krause <vkrause@kde.org>
2025-08-16 16:07:39 +0200

    Add import script to improve FlixBus/FlixTrain route types
    The feed uses generic rail and bus types, use long distance rail and coach
    service instead.

---
+++ scripts/eu-flixbus.lua
@@ -0,0 +1,17 @@
+-- SPDX-FileCopyrightText: Volker Krause <vkrause@kde.org>
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+-- map input route type to MOTIS clasz/output route type
+local route_type_map = {
+  [2] = { 2, 102 }, -- map FlixTrains from generic rail to long distance rail
+  [3] = { 3, 200 }  -- map FlixBuses from generic bus to coach service
+}
+
+function process_route(route)
+  local m = route_type_map[route:get_route_type()]
+  if m then
+    route:set_clasz(m[1])
+    route:set_route_type(m[2])
+  end
+  return true
+end
