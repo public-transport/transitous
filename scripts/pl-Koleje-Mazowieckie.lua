6bb58292a6a715f495979fba04245f4f5d36d7c1
Marcin Kasznia <kasmar00@gmail.com>
2025-09-29 23:36:30 +0200

    Add scripts for Polish trains (#1551)
    * Add scripts for Polish trains
    
    * fix json
    
    * fix REUSE

---
+++ scripts/pl-Koleje-Mazowieckie.lua
@@ -0,0 +1,17 @@
+-- SPDX-FileCopyrightText: Transitous Contributors
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+function process_route(route)
+    if route:get_id() == "ZL" then
+        route:set_clasz(10)
+        route:set_route_type(700)
+	elseif string.find(route:get_id(), "Z") then
+		route:set_clasz(10)
+		route:set_route_type(714) --rail replacement bus
+	elseif string.find(route:get_id(), "RE") then
+		route:set_clasz(5)
+	else
+		route:set_clasz(6)
+	end
+	return true
+end
---
+++ scripts/pl-RegioJet.lua
@@ -0,0 +1,10 @@
+-- SPDX-FileCopyrightText: Transitous Contributors
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+function process_route(route):
+    if route:get_id() == "WEB/11755" then
+        route:set_clasz(4)
+    else
+        route:set_clasz(2)
+    end
+end
