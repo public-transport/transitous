72074a093422f5a9791b8d460a5006f6ac96de93
applecuckoo <nufjoysb@duck.com>
2025-10-01 13:03:06 +1300

    au-vic/nz: misc. formatting scripts

---
+++ scripts/au-vic-Transport-Victoria-metro-trains.lua
@@ -0,0 +1,12 @@
+-- SPDX-FileCopyrightText: applecuckoo <nufjoysb@duck.com>
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+require "scripts.motis"
+
+function process_route(route)
+    -- set all routes with the name "Replacement Bus" as rail replacement buses
+    if route:get_short_name() == "Replacement Bus" then
+        route:set_route_type(714) -- rail replacement bus
+        route:set_clasz(BUS)
+    end
+end
