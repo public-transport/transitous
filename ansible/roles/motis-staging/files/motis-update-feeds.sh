870a386a87589e607d0af8dfe487d8bf42a7e593
Jonah Brüchert <jbb@kaidan.im>
2025-10-05 23:57:34 +0200

    ansible: motis-staging: Fix updating feeds after introduction of scripts

--- ansible/roles/motis-staging/files/motis-update-feeds.sh
+++ ansible/roles/motis-staging/files/motis-update-feeds.sh
@@ -7,7 +7,7 @@ echo "Copying new files…"
 
 cd /var/lib/motis
 chown -R motis:motis /var/lib/motis/
-sudo -u motis wget --mirror -l 1 --no-parent --no-directories --accept gtfs.zip --accept config.yml -e robots=off https://api.transitous.org/gtfs/ || true
+sudo -u motis wget --limit-rate=30m --mirror -l 2 --no-parent --cut-dirs=1 --no-host-directories --include-directories=gtfs,gtfs/scripts --accept gtfs.zip --accept .lua --accept config.yml --accept .import-running -e robots=off https://api.transitous.org/gtfs/ || true
 sudo -u motis sed -i 's/planet-latest.osm.pbf/liechtenstein-latest.osm.pbf/g' /var/lib/motis/config.yml
 sudo -u motis sed -i '/coastline:/d' /var/lib/motis/config.yml
 sudo -u motis /opt/motis/motis import -c /var/lib/motis/config.yml > /var/lib/motis/motis-import.log
