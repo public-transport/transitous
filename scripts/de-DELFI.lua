86dc0654d4c6eb88bbcd91282f03c2eb24fe47f7
Levin <github@17d.me>
2025-11-11 18:57:26 +0100

    Normalize line names for better mapping

--- scripts/de-DELFI.lua
+++ scripts/de-DELFI.lua
@@ -57,6 +57,8 @@ end
 function process_route(route)
     local agency_name = route:get_agency():get_name()
     local route_name = route:get_short_name()
+	-- remove spaces from route name for matching
+	route_name = route_name:gsub("%s+", "")
 	local original_route_color = route:get_color()
 	local original_route_text_color = route:get_text_color()
     if (original_route_color == 0 or original_route_text_color == 0) and colors[agency_name] and colors[agency_name][route_name] then
