d46578e8859fc8cf9ccf739ed72a0702c93cff21
Jonah Brüchert <jbb@kaidan.im>
2025-12-01 22:02:43 +0100

    website: Allow to specify the time (#1722)

--- website/src/MotisForward.purs
+++ website/src/MotisForward.purs
@@ -6,14 +6,16 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 module MotisForward where
 
-import Prelude (($), show)
-
-import Data.FormURLEncoded (fromArray, encode)
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Tuple (Tuple(..))
-import Data.Semigroup ((<>))
-
 import SearchBox
+
+import Data.DateTime (DateTime)
+import Data.Either (hush)
+import Data.FormURLEncoded (fromArray, encode)
+import Data.Formatter.DateTime (formatDateTime)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Semigroup ((<>))
+import Data.Tuple (Tuple(..))
+import Prelude (($), show)
 
 formatCoordinates :: Location -> String
 formatCoordinates { lat, lon } = show lat <> "," <> show lon
@@ -25,12 +27,14 @@ getIdentifier loc = case loc.type of
   "STOP" -> loc.id
   _ -> formatCoordinates loc
 
-toMotisWebUrl :: Location -> Location -> String
-toMotisWebUrl start destination = motisInstance <> "?" <> urlQuery
+toMotisWebUrl :: Location -> Location -> DateTime -> Boolean -> String
+toMotisWebUrl start destination dateTime arriveBy = motisInstance <> "?" <> urlQuery
   where
   urlQuery = fromMaybe "" $ encode $ fromArray
     [ Tuple "fromPlace" (Just (getIdentifier start))
     , Tuple "toPlace" (Just (getIdentifier destination))
     , Tuple "fromName" (Just start.name)
     , Tuple "toName" (Just destination.name)
+    , Tuple "time" (hush $ formatDateTime "YYYY-MM-DDTHH:mm" dateTime)
+    , Tuple "arriveBy" (Just $ show $ arriveBy)
     ]
