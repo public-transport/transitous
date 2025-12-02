a6e4b56b7b62640530b7ce777d95fe796f5f73c2
Jonah Brüchert <jbb@kaidan.im>
2025-12-02 11:17:15 +0100

    Fix reuse compliance

--- website/static/embed.js
+++ website/static/embed.js
@@ -1,3 +1,9 @@
+/*
+SPDX-FileCopyrightText: 2025 Jonah Brüchert <jbb@kaidan.im>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+*/
+
 function createTransitousWidget(parent, start, destination) {
 	let f = start ? window.TransitousWidget.location(start) : window.TransitousWidget.noLocation
 	let t = destination ? window.TransitousWidget.location(destination) : window.TransitousWidget.noLocation
