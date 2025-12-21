2ae81e0f6b7c3dffc2cb6d2a5b0f697fc25ba4b0
Jonah Brüchert <jbb@kaidan.im>
2025-12-21 17:37:16 +0100

    mdbify: Improve url matching

--- src/mdbify.py
+++ src/mdbify.py
@@ -5,26 +5,33 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from mobilitydatabase import Database
+from urllib.parse import urlparse
 
 import json
 import sys
 
+
+def canonicalize_url(url: str) -> str:
+    parsed = urlparse(url)
+    return parsed._replace(scheme="https", netloc=parsed.netloc.strip("www.")).geturl()
+
+
 mdb = Database.load()
 url_to_id = {}
 for feed in mdb.by_id.values():
-    url_to_id[feed["urls.direct_download"]] = feed["id"]
+    url_to_id[canonicalize_url(feed["urls.direct_download"])] = feed["id"]
 
 region = json.load(open(sys.argv[1], "r"))
 
 for source in region["sources"]:
     if source["type"] == "http":
-        mdb_id = url_to_id.get(source["url"])
+        mdb_id = url_to_id.get(canonicalize_url(source["url"]))
         if mdb_id:
             source["type"] = "mobility-database"
             source["mdb-id"] = mdb_id
             source.pop("url", None)
     if source["type"] == "url":
-        mdb_id = url_to_id.get(source["url"])
+        mdb_id = url_to_id.get(canonicalize_url(source["url"]))
         if mdb_id:
             source["type"] = "mobility-database"
             source["mdb-id"] = mdb_id
