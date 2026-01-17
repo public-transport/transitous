7cc7bd11e2508e6bd6cda5553f87b395a9c24622
Volker Krause <vkrause@kde.org>
2026-01-17 19:29:13 +0100

    Fix SNCB route types and display names (#1859)

---
+++ scripts/be-sncb.lua
@@ -0,0 +1,27 @@
+-- SPDX-FileCopyrightText: Volker Krause <vkrause@kde.org>
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+require "scripts.motis"
+
+function process_route(route)
+    if route:get_route_type() == 100 and route:get_short_name() == "L" then
+        route:set_clasz(REGIONAL_RAIL)
+        route:set_route_type(106)
+    end
+    if route:get_route_type() == 100 and route:get_short_name():sub(1, 1) == "S" then
+        route:set_clasz(SUBURBAN)
+        route:set_route_type(109)
+    end
+end
+
+local use_trip_short_name = {
+    ["IC"] = true,
+    ["L"] = true,
+    ["P"] = true
+}
+
+function process_trip(trip)
+    if use_trip_short_name[trip:get_route():get_short_name()] then
+        trip:set_display_name(trip:get_route():get_short_name() .. " " .. trip:get_short_name())
+    end
+end
