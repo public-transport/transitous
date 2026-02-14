9838cb316710d8b6a34df96a66d5ee0c511a9d78
Volker Krause <vkrause@kde.org>
2026-02-14 14:12:28 +0100

    Fix Blablacar Bus route type
    MOTIS v2.8 doesn't have the coach detection heuristic anymore, so those
    were now only shown as regular busses.

---
+++ scripts/eu-blablacarbus.lua
@@ -0,0 +1,12 @@
+-- SPDX-FileCopyrightText: Volker Krause <vkrause@kde.org>
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+require "scripts.motis"
+
+-- change generic bus type to long-distance coach
+function process_route(route)
+    if route:get_route_type() == 3 then
+        route:set_clasz(COACH)
+        route:set_route_type(200)
+  end
+end
