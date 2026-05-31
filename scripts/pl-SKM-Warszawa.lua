9d27adf4b7584e598ca3223b325981da56b8d6a7
Marcin Kasznia <kasmar00@gmail.com>
2026-05-31 11:54:05 +0200

    remove unused scripts

--- scripts/pl-SKM-Warszawa.lua
+++
@@ -1,6 +0,0 @@
--- SPDX-FileCopyrightText: Transitous Contributors
--- SPDX-License-Identifier: AGPL-3.0-or-later
-
-function process_route(route)
-  	route:set_route_type(109) -- SUBURBAN_RAILWAY_SERVICE in future motis
-end
