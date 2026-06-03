d311a680add5bb75a8b480d777e6b5e661828185
Marcin Kasznia <kasmar00@gmail.com>
2026-06-03 10:21:55 +0200

    fix REUSE

--- scripts/pl-polish-trains.lua
+++ scripts/pl-polish-trains.lua
@@ -1,3 +1,6 @@
+-- SPDX-FileCopyrightText: Transitous Contributors
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 function process_trip(trip)
     trip:set_display_name(trip:get_route():get_short_name() .. " " .. trip:get_short_name())
 end
