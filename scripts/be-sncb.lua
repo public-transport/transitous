89236a34e6af077e03630c0a5b0e309aa3a7c34f
Kenny <dan@dcdev.ro>
2026-06-29 07:44:56 +0200

    Add colors for the Belgian S Trains (#2263)

--- scripts/be-sncb.lua
+++ scripts/be-sncb.lua
@@ -1,6 +1,8 @@
 -- SPDX-FileCopyrightText: Volker Krause <vkrause@kde.org>
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
+local colors = require 'scripts.be-sncb-colors'
+
 function process_route(route)
     if route:get_route_type() == 100 and route:get_short_name() == "L" then
         route:set_route_type(106)
@@ -8,6 +10,17 @@ function process_route(route)
     if route:get_route_type() == 100 and route:get_short_name():sub(1, 1) == "S" then
         route:set_route_type(109)
     end
+
+    local route_name = route:get_short_name()
+	-- remove spaces from route name for matching
+	route_name = route_name:gsub("%s+", "")
+	local original_route_color = route:get_color()
+	local original_route_text_color = route:get_text_color()
+	if (original_route_color == 0 or original_route_text_color == 0) and colors[route_name] then
+		local colors = colors[route_name]
+		route:set_color(colors.color)
+		route:set_text_color(colors.text_color)
+	end
 end
 
 local use_trip_short_name = {
