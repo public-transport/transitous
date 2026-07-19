64e77ab41b5cc7ffac1dd53d92d6d54a54b9ee01
Milen-Joan <44903989+BPilot253@users.noreply.github.com>
2026-07-20 00:29:38 +0300

    bg: BDZ extended route types + new realtime feed (#2312)

--- scripts/bg-bdz.lua
+++ scripts/bg-bdz.lua
@@ -1,6 +1,16 @@
 -- SPDX-FileCopyrightText: Transitous Contributors
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
+function process_route(route)
+    if table.find({"BV", "MBV"}, route:get_id()) then
+        route:set_route_type(102)
+    elseif table.find({"PV", "KPV"}, route:get_id()) then
+        route:set_route_type(106)
+    elseif route:get_id() == "A" then
+        route:set_route_type(714)
+    end
+end
+
 function process_trip(trip)
     trip:set_display_name(trip:get_route():get_short_name() .. " " .. trip:get_short_name())
 end
