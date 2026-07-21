47a616536dffc4c85ff0f856be5e1db335705116
Jonah Brüchert <jbb@kaidan.im>
2026-07-21 21:00:25 +0200

    bg: bdz: Fix script

--- scripts/bg-bdz.lua
+++ scripts/bg-bdz.lua
@@ -2,9 +2,9 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 function process_route(route)
-    if table.find({"BV", "MBV"}, route:get_id()) then
+    if route:get_id() == "BV" or route:get_id() == "MBV" then
         route:set_route_type(102)
-    elseif table.find({"PV", "KPV"}, route:get_id()) then
+    elseif route:get_id() == "PV" or route:get_id() == "KPV" then
         route:set_route_type(106)
     elseif route:get_id() == "A" then
         route:set_route_type(714)
