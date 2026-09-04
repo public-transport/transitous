ed238836cb2729c6eaec62a4387cf6dc99330721
luna-cant-code <223513004+luna-cant-code@users.noreply.github.com>
2026-09-05 01:25:33 +0200

    call SkyTrain by its name (#2447)

--- scripts/de-DELFI.lua
+++ scripts/de-DELFI.lua
@@ -63,6 +63,11 @@ end
 function process_route(route)
     local agency_name = route:get_agency():get_name()
     local route_name = route:get_short_name()
+    -- Change SkyTrain Düsseldorf to its full name
+    if  route:get_short_name() == 'SkyT' then
+        route:set_short_name("SkyTrain")
+        trip:set_route_type(405) --monorail under extended GTFS route types
+    end
 	-- remove spaces from route name for matching
 	route_name = route_name:gsub("%s+", "")
 	local original_route_color = route:get_color()
